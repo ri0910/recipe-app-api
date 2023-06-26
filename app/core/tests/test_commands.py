8cc34b05c7a8fce6b3131cbdf2ec55e64896f84b
Riya karmakar <riyakarmakar.kolkata@gmail.com>
2023-06-26 20:32:30 +0530

    Added some changes for recipe app

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,27 @@
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as psycopg2_error
+from django.db.utils import OperationalError
+from django.core.management import call_command
+from django.test import SimpleTestCase
+
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandCheck(SimpleTestCase):
+
+    '''Test commands'''
+
+    def test_wait_for_db_ready(self, patched_check):
+        '''Test waiting for database ready'''
+        patched_check.return_value = True
+        call_command('wait_for_db')
+        patched_check.assert_called_once_with(databases = ['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self,patched_sleep,patched_check):
+        '''Test waiting for database when getting operationalerror'''
+        patched_check.side_effect = [psycopg2_error]*2+\
+            [OperationalError]*3+[True]
+        call_command('wait_for_db')
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases = ['default'])
